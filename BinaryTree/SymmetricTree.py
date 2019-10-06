f795d30f5aafc64dec65cc4b914f7dd5f8628b81
tcandzq <2448424636@qq.com>
2019-10-06 21:18:47 +0800

    新增leetcode101对称二叉树+20191006+陶超

--- BinaryTree/SymmetricTree.py
+++ BinaryTree/SymmetricTree.py
@@ -22,6 +22,9 @@
 说明:
 
 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+
+参考:https://leetcode-cn.com/problems/symmetric-tree/solution/dui-cheng-er-cha-shu-by-leetcode/
+
 """
 class TreeNode:
     def __init__(self, x):
@@ -30,7 +33,60 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    # 递归解法
+    # 递归解法(中序遍历) 错误
     def isSymmetric1(self, root: TreeNode) -> bool:
         nums = []
-        pass
+        self.inorderTraversal(root,nums)
+        return nums == list(reversed(nums))
+    # 中序遍历
+    def inorderTraversal(self,root,nums):
+        if not root:
+            return
+        self.inorderTraversal(root.left,nums)
+        nums.append(root.val)
+        self.inorderTraversal(root.right,nums)
+    #  递归解法 正确
+    def isSymmetric2(self, root: TreeNode) -> bool:
+        if not root:  # 注意这里的异常情况
+            return True
+        return self.helper(root.left,root.right)
+
+    def helper(self,p,q):
+        if p and q and p.val != q.val:
+            return False
+        if (p and not q) or (q and not p):
+            return False
+        if (not p) and (not q):
+            return True
+        print(p.val,q.val)
+        return self.helper(p.left,q.right) and self.helper(p.right,q.left)
+
+if __name__ == '__main__':
+    # root = TreeNode(1)
+    # node1 = TreeNode(2)
+    # node2 = TreeNode(2)
+    # node3 = TreeNode(3)
+    # node4 = TreeNode(4)
+    # node5 = TreeNode(4)
+    # node6 = TreeNode(3)
+    #
+    # root.left = node1
+    # root.right = node2
+    # node1.left = node3
+    # node1.right = node4
+    # node2.left = node5
+    # node2.right = node6
+
+    root = TreeNode(1)
+    node1 = TreeNode(2)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    node4 = TreeNode(3)
+
+    root.left = node1
+    root.right = node2
+    node1.right = node3
+    node2.right = node4
+
+    solution = Solution()
+    print(solution.isSymmetric2(root))

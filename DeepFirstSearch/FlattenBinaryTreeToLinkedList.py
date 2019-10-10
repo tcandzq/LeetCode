d7e4905ea19a0a3767a4ff3fe2e8dae401837fac
tcandzq <2448424636@qq.com>
2019-10-10 23:52:01 +0800

    新增leetcode114二叉树展开为链表+20191010+陶超

---
+++ DeepFirstSearch/FlattenBinaryTreeToLinkedList.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/10 22:44
+# @Author  : tc
+# @File    : FlattenBinaryTreeToLinkedList.py
+"""
+题号114 二叉树展开为链表
+给定一个二叉树，原地将它展开为链表。
+
+例如，给定二叉树
+
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+将其展开为：
+
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6
+
+参考:https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/solution/flatten-binary-tree-to-linked-list-qian-xu-bian-li/
+
+一看就是先序遍历,boring
+
+"""
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    pre = None
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return
+        if self.pre:
+            self.pre.right = root
+            self.pre.left =  None
+        self.pre = root
+        right = root.right
+        self.flatten(root.left)
+        self.flatten(right)
+
+
+
--- Stack/TrappingRainWater.py
+++ Stack/TrappingRainWater.py
@@ -30,14 +30,16 @@ class Solution:
         max_left = 0
         max_right = [0 for _ in range(len(height))]
         for i in range(len(height) - 2,-1,-1):
+            print(max_right)
             max_right[i] = max(max_right[i+1],height[i+1])
+        print(max_right)
         for i in range(1,len(height) - 1):
             max_left = max(max_left,height[i-1])
             _min = min(max_left,max_right[i])
             if _min > height[i]:
+                print(_min)
                 sum = sum + (_min - height[i])
         return sum
-
 
 if __name__ == '__main__':
     height = [0,1,0,2,1,0,1,3,2,1,2,1]

bf81ea5ff5e55a4e9f00454f08aa8d2fc50e6c01
tcandzq <2448424636@qq.com>
2019-10-17 23:55:10 +0800

    leetcode39增加答案+20191017+陶超

--- Backtracking/CombinationSum.py
+++ Backtracking/CombinationSum.py
@@ -30,12 +30,47 @@ candidates 中的数字可以无限制重复被选取。
   [2,3,3],
   [3,5]
 ]
+
+"做搜索、回溯问题的套路是画图，代码其实就是根据画出的树形图写出来的。"
+
+又是一套拥有标准模板写法的题。
+
+参考:https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+
 """
 from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        tmp = []
-        for num in candidates:
-            tmp.append(target // num)
-        for i in tmp:
-            
+        size = len(candidates)
+        if size == 0:
+            return []
+
+        # 剪枝的前提是数组元素排序
+        # 深度深的边不能比深度浅的边还小
+        # 要排序的理由：1、前面用过的数后面不能再用；2、下一层边上的数不能小于上一层边上的数。
+        candidates.sort()
+        # 在遍历的过程中记录路径，一般而言它是一个栈
+        path = []
+        res = []
+        # 注意要传入 size ，在 range 中， size 取不到
+        self.__dfs(candidates, 0, size, path, res, target)
+        return res
+
+    def __dfs(self, candidates, begin, size, path, res, target):
+        # 先写递归终止的情况
+        if target == 0:
+            # Python 中可变对象是引用传递，因此需要将当前 path 里的值拷贝出来
+            # 或者使用 path.copy()
+            res.append(path[:])
+
+        for index in range(begin, size):
+            residue = target - candidates[index]
+            # “剪枝”操作，不必递归到下一层，并且后面的分支也不必执行
+            if residue < 0:
+                break
+            path.append(candidates[index])
+            # 因为下一层不能比上一层还小，起始索引还从 index 开始
+            self.__dfs(candidates, index, size, path, res, residue)
+            path.pop()
+
+

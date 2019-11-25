924ea59367a90e264cbb867c6fad6fb48bf415d7
tcandzq <2448424636@qq.com>
2019-11-25 22:38:49 +0800

    新增堆排序的python实现+20191125+python

--- Algorithm/heap.py
+++ Algorithm/heap.py
@@ -4,74 +4,114 @@
 # @Author  : tc
 # @File    : heap.py
 """
-满二叉树:除了最下面一层之外,其余各层的结点个数都达到了当前层能达到的最大点数,并且最下面一层从左至右连续存在若干结点,而这
-些连续结点全部不存在。
-说人话:完全二叉树从根结点到倒数第二层满足完美二叉树，最后一层可以不完全填充，其叶子结点都靠左对齐。
+实现大顶堆
+参考:https://www.liwei.party/2019/01/10/algorithms-and-data-structures/priority-queue/
 """
+class MaxHeap:
+    def __init__(self, capacity):
+        # 我们这个版本的实现中，0 号索引是不存数据的，这一点一定要注意
+        # 因为数组从索引 1 开始存放数值
+        # 所以开辟 capacity + 1 这么多大小的空间
+        self.data = [None for _ in range(capacity + 1)]
+        # 当前堆中存储的元素的个数
+        self.count = 0
+        # 堆中能够存储的元素的最大数量（为简化问题，不考虑动态扩展）
+        self.capacity = capacity
 
-"""
-向下调整的过程如下:
-总是将当前结点V与它的左右孩子比较(如果有的话),假设孩子中存在权值比结点V的权值大的,就将其中权值最大的那个孩子结点与结点V交换;
-交换完毕后继续让结点V和孩子比较,直到结点V的孩子结点的权值都比结点V的权值小或者不存在孩子结点。
+    def size(self):
+        """
+        返回最大堆中的元素的个数
+        :return:
+        """
+        return self.count
 
-"""
-def down_adjust(low,high,heap):
-    i = low
-    j = i * 2  #i为欲调整结点,j为其左孩子
-    while j <= high: #存在孩子结点
-        #如果右孩子存在,且右孩子的值大于左孩子
-        if j+1 <= high and heap[j+1] > heap[j]:
-            j += 1    #让j存储右孩子的位置
+    def is_empty(self):
+        """
+        返回最大堆中的元素是否为空
+        :return:
+        """
+        return self.count == 0
 
-        #如果孩子中最大的权值比欲调整的结点i大
-        if heap[j] > heap[i]:
-            heap[j], heap[i] = heap[i], heap[j] #交换最大权值的孩子与欲调整结点i
-            i = j   #保持i为欲调整结点,j为i的左孩子
-            j = i*2
-        else:
-            break
+    def insert(self, item):
+        if self.count + 1 > self.capacity:
+            raise Exception('堆的容量不够了')
+        self.count += 1
+        self.data[self.count] = item
+        # 考虑将它上移
+        self.__swim(self.count)
 
-#建堆的过程
-"""
-假设序列中的元素个数为n,由于完全二叉树的叶子结点个数为[n/2]
-"""
-def create_heap(n,heap):
-    for i in range(n//2, 0, -1):
-        down_adjust(i,n,heap)
+    def __shift_up(self, k):
+        # 有索引就要考虑索引越界的情况，已经在索引 1 的位置，就没有必要上移了
+        while k > 1 and self.data[k // 2] < self.data[k]:
+            self.data[k // 2], self.data[k] = self.data[k], self.data[k // 2]
+            k //= 2
 
-#删除推顶元素
-def delete_top(heap,n):
-    heap[1] =heap[n]  # 用最后一个元素覆盖堆顶元素
-    n -= 1           # 让元素个数减1
-    down_adjust(1,n,heap) #向下调整堆顶元素
+    def __swim(self, k):
+        # 上浮，与父结点进行比较
+        temp = self.data[k]
+        # 有索引就要考虑索引越界的情况，已经在索引 1 的位置，就没有必要上移了
+        # 新加入的元素只和父结点比较大小（无须和它的兄弟结点比较），只要比父结点大（严格大于），就往上走
+        while k > 1 and self.data[k // 2] < temp:
+            self.data[k] = self.data[k // 2]
+            k //= 2
+        self.data[k] = temp
+
+    def extract_max(self):
+        if self.count == 0:
+            raise Exception('堆里没有可以取出的元素')
+        ret = self.data[1]
+        self.data[1], self.data[self.count] = self.data[self.count], self.data[1]
+        self.count -= 1
+        self.__sink(1)
+        return ret
+
+    def __shift_down(self, k):
+        # 只要有左右孩子，左右孩子只要比自己大，就交换
+        while 2 * k <= self.count:
+            # 如果这个元素有左边的孩子
+            j = 2 * k
+            # 如果有右边的孩子，大于左边的孩子，就好像左边的孩子不存在一样
+            if j + 1 <= self.count and self.data[j + 1] > self.data[j]:
+                j = j + 1
+            if self.data[k] >= self.data[j]:
+                break
+            self.data[k], self.data[j] = self.data[j], self.data[k]
+            k = j
+
+    def __sink(self, k):
+        # 下沉
+        temp = self.data[k]
+        # 只要它有孩子，注意，这里的等于号是十分关键的
+        while 2 * k <= self.count:
+            j = 2 * k
+            # 如果它有右边的孩子，并且右边的孩子大于左边的孩子
+            if j + 1 <= self.count and self.data[j + 1] > self.data[j]:
+                # 右边的孩子胜出，此时可以认为没有左孩子
+                j += 1
+            # 如果当前的元素的值，比右边的孩子节点要大，则逐渐下落的过程到此结束
+            if temp >= self.data[j]:
+                break
+            # 否则，交换位置，继续循环
+            self.data[k] = self.data[j]
+            k = j
+        self.data[k] = temp
 
 
-# 向上调整操作
-# 对heap数组在[low,high]范围内进行向上调整
-# 其中low一般设置为1,high表示欲调整结点的数组下标
-def up_adjust(low,high,heap):
-    i = high   # i为欲调整的结点
-    j = i // 2  # j为其父结点
-    while j >= low:  # j在[low,high]的范围内
-        if heap[j] < heap[i]:  # 父亲结点的权值小于欲调整结点的权值
-            heap[j],heap[i] = heap[i],heap[j]  #交换父结点和欲调整结点
-            i = j  # 保持i为欲调整结点,j为i的父结点
-            j = i // 2
-        else:
-            break
+if __name__ == '__main__':
+    max_heap = MaxHeap(6)
+    max_heap.insert(3)
+    print(max_heap.data[1])
+    max_heap.insert(5)
+    print(max_heap.data[1])
+    max_heap.insert(1)
+    print(max_heap.data[1])
+    max_heap.insert(8)
+    print(max_heap.data[1])
+    max_heap.insert(7)
+    print(max_heap.data[1])
+    max_heap.insert(12)
 
+    while not max_heap.is_empty():
+        print('取出', max_heap.extract_max())
 
-#添加元素x
-def insert(x,heap,n):
-    n += 1    # 让元素个数加1
-    heap[n] = x  # 将数组末位赋值为x
-    up_adjust(1,n,heap) # 向上调整新加入的结点n
-
-
-#堆排序
-def heap_sort(n,heap):
-    create_heap(n,heap)   # 建堆
-    for i in range(n,2,-1):  # 倒着枚举,直到堆中只有一个元素
-        heap[i],heap[1] = heap[1],heap[i]  # 交换heap[i]与堆顶
-        down_adjust(1,i-1,heap)  # 调整堆顶
 

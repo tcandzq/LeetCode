addb8527d8dcdce083a8bea04a65aa1139e8c846
tcandzq <2448424636@qq.com>
2019-12-02 23:24:07 +0800

    新增leetcode334递增的三元子序列+20191202+python

--- DynamicProgramming/IncreasingTripletSubsequence.py
+++ DynamicProgramming/IncreasingTripletSubsequence.py
@@ -23,6 +23,8 @@
 输入: [5,4,3,2,1]
 输出: false
 
+思路参考:https://leetcode-cn.com/problems/increasing-triplet-subsequence/solution/jie-jian-di-300ti-de-si-lu-onshi-jian-fu-za-du-o1k/
+
 """
 from typing import List
 
@@ -30,15 +32,16 @@ class Solution:
     def increasingTriplet(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return False
-        dp = [1] * len(nums)
-        for i in range(1,len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = dp[j] + 1
-                if dp[i] >= 3:
-                    return True
+        first = float('inf')
+        second = float('inf')
+        for num in nums:
+            if num <= first:
+                first = num  # first 始终记录最小的元素
+            elif num <= second:
+                second = num  # 接下来不断更新first，同时保持second尽可能的小。
+            else:
+                return True  # 如果下一个元素比second大，说明找到了三元组。
         return False
-
 
 if __name__ == '__main__':
     nums = [2,1,5,0,3]

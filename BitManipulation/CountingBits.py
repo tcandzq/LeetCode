a40381c40f6db0fddef22981bb070e28d2da9a76
tcandzq <2448424636@qq.com>
2019-12-15 22:28:50 +0800

    新增191位1的个数+20191215+python

--- BitManipulation/CountingBits.py
+++ BitManipulation/CountingBits.py
@@ -30,11 +30,12 @@ class Solution:
                 count += 1
             res.append(count)
         return res
+
     # 优化后的代码
     def countBits2(self, num: int) -> List[int]:
         res = [0] * (num + 1)
         for i in range(1,num+1):
-            res[i] = res[i & (i -1)] + 1
+            res[i] = res[i & (i - 1)] + 1
         return res
 
 if __name__ == '__main__':

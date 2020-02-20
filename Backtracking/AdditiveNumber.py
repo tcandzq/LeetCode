60b7a8b48074f5e8e063af865e80d3c366ec3ea0
tcandzq <2448424636@qq.com>
2020-02-20 13:33:27 +0800

    新增 leetcode 435 无重叠区间+2020219+python

--- Backtracking/AdditiveNumber.py
+++ Backtracking/AdditiveNumber.py
@@ -23,31 +23,87 @@
 输出: true
 解释: 累加序列为: 1, 99, 100, 199。1 + 99 = 100, 99 + 100 = 199
 
+第一个数定义为num[:i]，包括num[0],但不包括num[i]。
+当第一个数和任意长度的第二个数相加，一定会得到一个和，这个和的长度至少和第一个数一样长。
+因此，第一个数的长度不可能超过给定字符串num长度的一半
+因此，当i达到(n/2)+1时，我们可以停止迭代
+注意到，当len(num) < 2时，循环退出
+
+代码参考：https://leetcode.com/problems/additive-number/discuss/75567/Java-Recursive-and-Iterative-Solutions
+
 """
 class Solution:
+    # 带注释详细版
     def isAdditiveNumber(self, num: str) -> bool:
         n = len(num)
-        for i in range(1, n//2 + 1):
-            if num[0] == '0' and i > 1:
-                return False
-            x1 = int(num[:i])
-            for j in range(1,n):
-                if max(j,i) > n - i - j:
+        # 迭代所有可能的第一个数
+        # 第一个数定义为num[:i]
+        for i in range(1,  n//2 + 1):
+            # 迭代所有可能的第二个数
+            # 第二个数定义为num[i:i+j]
+            # 当第二个数加到任意长度的第一个数，肯定会得到一个长度至少为max(i,j)的和sum
+            # n是num的长度，i是第一个数，j是第二个数，那么(n - i - j)就是sum对应的数字
+            # 由于sum的长度至少为max(i,j)，因此当max(i,j)大于n - i - j时，我们可以停止迭代
+            for j in range(1, n):
+                if max(i, j) > n - i - j:
                     break
-                if num[i] == '0' and j > 1:
-                    break
-                x2 = int(num[i:i+j])
-                if self.is_valid(x1,x2,j+i,num):
+                if self.is_valid(i, j, num):
                     return True
         return False
 
-    def is_valid(self, x1: int, x2: int, start: int, num: str):
+    def is_valid(self, i: int, j: int,  num: str): # 第一个数的长度是i，第二个数的长度是j,从num[i]开始
+        # 检查第一个数中的leading zero
+        # 第一个数允许为0，但此时它的长度不能超过1
+        if num[0] == '0' and i > 1: return False
+        # 第二个数允许为0，但此时它的长度不能超过1
+        if num[i] == '0' and j > 1 : return False
+        # 第一个数
+        b1 = int(num[:i])
+        # 第二个数
+        b2 = int(num[i:i+j])
+        # (1)计算第一个数和第二个数的和sum
+        # (2)将第二个数更新为sum
+        # (3)将第一个数更新为未更新前的第二个数
+        # (4)检查num里的下一个数是不是sum
+        # (5) 重复循环，直到下一个sum的起始下标超过了num的最后一个索引
+        offset = i + j
+        while offset < len(num):
+            # 第二个数变成了第一个数和第二个数的和
+            b2 = b2 + b1
+            # 第一个数变成了第二个数
+            b1 = b2 - b1
+            _sum = str(b2)
+            # 检查num的下一个数字
+            if not num.startswith(_sum, offset): return False
+            # offset的增加量是上一次的和_sum的长度
+            offset += len(_sum)
+        return True
+
+    # 不带注释+尾递归版
+    def isAdditiveNumber2(self, num: str) -> bool:
+        n = len(num)
+        for i in range(1, n // 2 + 1):
+            if num[0] == '0' and i > 1:
+                return False
+            x1 = int(num[:i])
+            for j in range(1, n):
+                if max(j, i) > n - i - j:
+                    break
+                if num[i] == '0' and j > 1:
+                    break
+                x2 = int(num[i:i + j])
+                if self.is_valid2(x1, x2, j + i, num):
+                    return True
+        return False
+
+    def is_valid2(self, x1: int, x2: int, start: int, num: str):
         if start == len(num):
             return True
         x2 = x2 + x1
         x1 = x2 - x1
         _sum = str(x2)
-        return num.startswith(_sum,start) and self.is_valid(x1,x2,start+len(_sum), num)
+        return num.startswith(_sum, start) and self.is_valid2(x1, x2, start + len(_sum), num)
+
 
 if __name__ == '__main__':
     num = "199100199"
---
+++ Greedy/NonOverlappingIntervals.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# @File    : NonOverlappingIntervals.py
+# @Date    : 2020-02-20
+# @Author  : tc
+"""
+题号 435 无重叠区间
+给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+
+注意:
+
+可以认为区间的终点总是大于它的起点。
+区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+示例 1:
+
+输入: [ [1,2], [2,3], [3,4], [1,3] ]
+
+输出: 1
+
+解释: 移除 [1,3] 后，剩下的区间没有重叠。
+示例 2:
+
+输入: [ [1,2], [1,2], [1,2] ]
+
+输出: 2
+
+解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+示例 3:
+
+输入: [ [1,2], [2,3] ]
+
+输出: 0
+
+解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+
+代码参考：https://leetcode.com/problems/non-overlapping-intervals/discuss/91721/Short-Ruby-and-Python
+
+算法参考：https://leetcode-cn.com/problems/non-overlapping-intervals/solution/tan-xin-suan-fa-zhi-qu-jian-diao-du-wen-ti-by-labu/
+
+"""
+from typing import List
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        end = float('-inf')
+        erased = 0
+        intervals.sort(key=lambda i:i[1])
+        print(intervals)
+        for i in intervals:
+            if i[0] >= end:
+                end = i[1]
+            else:
+                erased += 1
+        return erased
+
+
+if __name__ == '__main__':
+    intervals = [[1, 2], [2, 3], [3, 4], [1, 3]]
+    solution = Solution()
+    print(solution.eraseOverlapIntervals(intervals))

38b57f0d2a1bb3da250db1470d928f93c3891304
tcandzq <2448424636@qq.com>
2020-03-17 23:48:32 +0800

    新增 leetcode 904. 水果成篮+2020317+python

--- DeepFirstSearch/NumberOfClosedIslands.py
+++ DeepFirstSearch/NumberOfClosedIslands.py
@@ -44,9 +44,37 @@
 
 1 <= grid.length, grid[0].length <= 100
 0 <= grid[i][j] <=1
+
+参考：https://leetcode.com/problems/number-of-closed-islands/discuss/425122/Python-easy-understand-DFS-solution
+
 """
 from typing import List
 
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        pass
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+
+        def dfs(i, j, val):
+            if 0 <= i < m and 0 <= j < n and grid[i][j] == 0:
+                grid[i][j] = val
+                dfs(i, j + 1, val)
+                dfs(i + 1, j, val)
+                dfs(i - 1, j, val)
+                dfs(i, j - 1, val)
+
+        for i in range(m):
+            for j in range(n):
+                if (i == 0 or j == 0 or i == m - 1 or j == n - 1) and grid[i][j] == 0:
+                    dfs(i, j, 1)
+
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    dfs(i, j, 1)
+                    res += 1
+
+        return res

ba8ae56eabe8427011ba1e631506d8539db5f367
tcandzq <2448424636@qq.com>
2020-03-27 23:56:17 +0800

    新增 leetcode524. 通过删除字母匹配到字典里最长单词+2020327+python

---
+++ Sort/LongestWordInDictionaryThroughDeleting.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @File    : LongestWordInDictionaryThroughDeleting.py
+# @Date    : 2020-03-27
+# @Author  : tc
+"""
+题号 524. 通过删除字母匹配到字典里最长单词
+给定一个字符串和一个字符串字典，找到字典里面最长的字符串，该字符串可以通过删除给定字符串的某些字符来得到。如果答案不止一个，返回长度最长且字典顺序最小的字符串。如果答案不存在，则返回空字符串。
+
+示例 1:
+
+输入:
+s = "abpcplea", d = ["ale","apple","monkey","plea"]
+
+输出:
+"apple"
+示例 2:
+
+输入:
+s = "abpcplea", d = ["a","b","c"]
+
+输出:
+"a"
+说明:
+
+所有输入的字符串只包含小写字母。
+字典的大小不会超过 1000。
+所有输入的字符串长度不会超过 1000。
+"""
+from typing import List
+
+class Solution:
+    def findLongestWord(self, s: str, d: List[str]) -> str:
+        pass

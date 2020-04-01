60a3f1bc144c0a688200771f9f9c2f319d79d47d
tcandzq <2448424636@qq.com>
2020-04-01 23:58:04 +0800

    新增 剑指offer25. 合并两个排序的链表+20200401+python

---
+++ CodingInterview/25_MergeSortedLists.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# @File    : 25_MergeSortedLists.py
+# @Date    : 2020-04-01
+# @Author  : tc
+"""
+面试题25. 合并两个排序的链表
+输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+
+示例1：
+
+输入：1->2->4, 1->3->4
+输出：1->1->2->3->4->4
+限制：
+
+0 <= 链表长度 <= 1000
+"""
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        pass
+    
--- README.md
+++ README.md
@@ -393,3 +393,4 @@
 ## [剑指offer系列](CodingInterview/)
 * - [面试题21.调整数组顺序使奇数位于偶数前面.py](SlidingWindow/LongestRepeatingCharacterReplacement.py)
 * - [面试题48.最长不含重复字符的子字符串.py](SlidingWindow/LongestRepeatingCharacterReplacement.py)
+* - [面试题25.合并两个排序的链表.py](SlidingWindow/LongestRepeatingCharacterReplacement.py)

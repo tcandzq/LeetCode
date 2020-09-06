4102f5fcb6bc287a60780fd9a738aa51c4953190
tcandzq <2448424636@qq.com>
2020-09-07 00:56:48 +0800

    新增leetcode931.下降路径最小和+20200907

---
+++ DynamicProgramming/MinimumFallingPathSum.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# @File    : MinimumFallingPathSum.py
+# @Date    : 2020-09-07
+# @Author  : tc
+"""
+题号 931. 下降路径最小和
+给定一个方形整数数组 A，我们想要得到通过 A 的下降路径的最小和。
+
+下降路径可以从第一行中的任何元素开始，并从每一行中选择一个元素。在下一行选择的元素和当前行所选元素最多相隔一列。
+
+
+
+示例：
+
+输入：[[1,2,3],[4,5,6],[7,8,9]]
+输出：12
+解释：
+可能的下降路径有：
+[1,4,7], [1,4,8], [1,5,7], [1,5,8], [1,5,9]
+[2,4,7], [2,4,8], [2,5,7], [2,5,8], [2,5,9], [2,6,8], [2,6,9]
+[3,5,7], [3,5,8], [3,5,9], [3,6,8], [3,6,9]
+和最小的下降路径是 [1,4,7]，所以答案是 12。
+
+
+
+提示：
+1 <= A.length == A[0].length <= 100
+-100 <= A[i][j] <= 100
+
+参考：https://leetcode.com/problems/minimum-falling-path-sum/discuss/186666/C%2B%2BJava-4-lines-DP
+
+"""
+from typing import List
+
+class Solution:
+    def minFallingPathSum(self, A: List[List[int]]) -> int:
+        N = len(A)
+        for i in range(1, N):
+            for j in range(N):
+                A[i][j] += min(A[i - 1][k] for k in (j - 1, j, j + 1) if 0 <= k < N)
+        return min(A[-1])
+
+if __name__ == '__main__':
+    pass
--- README.md
+++ README.md
@@ -146,6 +146,7 @@
 * - [最长快乐字符串.py](DynamicProgramming/ConstrainedSubsequenceSum.py)
 * - [跳跃游戏V.py](DynamicProgramming/JumpGameV.py)
 * - [有效的快递序列数目.py](DynamicProgramming/CountAllValidPickupAndDeliveryOptions.py)
+* - [下降路径最小和.py](DynamicProgramming/MinimumFallingPathSum.py)
 ## [双指针(及其相关结构)](TwoPointer/)
 * - [长度最小的子数组.py](TwoPointer/MinimumSizeSubarraySum.py)
 * - [最小覆盖子串.py](TwoPointer/MinimumWindowSubstring.py)

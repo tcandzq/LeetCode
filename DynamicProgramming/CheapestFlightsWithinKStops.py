ef31e64421bfc8df69f96971c8e89b6108ba5cf1
tcandzq <2448424636@qq.com>
2020-09-13 23:42:31 +0800

    新增leetcode787. K站中转内最便宜的航班+20200913

---
+++ DynamicProgramming/CheapestFlightsWithinKStops.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# @File    : CheapestFlightsWithinKStops.py
+# @Date    : 2020-09-13
+# @Author  : tc
+"""
+题号 787. K站中转内最便宜的航班
+有 n 个城市通过 m 个航班连接。每个航班都从城市 u 开始，以价格 w 抵达 v。
+
+现在给定所有的城市和航班，以及出发城市 src 和目的地 dst，你的任务是找到从 src 到 dst 最多经过 k 站中转的最便宜的价格。 如果没有这样的路线，则输出 -1。
+
+示例 1：
+详见：https://leetcode-cn.com/problems/cheapest-flights-within-k-stops/
+
+
+输入:
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 1
+输出: 200
+解释:
+城市航班图如下
+
+
+从城市 0 到城市 2 在 1 站中转以内的最便宜价格是 200，如图中红色所示。
+示例 2：
+
+输入:
+n = 3, edges = [[0,1,100],[1,2,100],[0,2,500]]
+src = 0, dst = 2, k = 0
+输出: 500
+解释:
+城市航班图如下
+
+
+从城市 0 到城市 2 在 0 站中转以内的最便宜价格是 500，如图中蓝色所示。
+
+
+提示：
+
+n 范围是 [1, 100]，城市标签从 0 到 n - 1.
+航班数量范围是 [0, n * (n - 1) / 2].
+每个航班的格式 (src, dst, price).
+每个航班的价格范围是 [1, 10000].
+k 范围是 [0, n - 1].
+航班没有重复，且不存在环路
+
+参考：https://leetcode.com/problems/cheapest-flights-within-k-stops/discuss/115541/JavaPython-Priority-Queue-Solution
+"""
+from typing import List
+import collections
+import heapq
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        f = collections.defaultdict(dict)
+        for a, b, p in flights:
+            f[a][b] = p
+        heap = [(0, src, K + 1)]
+        while heap:
+            p, i, k = heapq.heappop(heap)
+            if i == dst:
+                return p
+            if k > 0:
+                for j in f[i]:
+                    heapq.heappush(heap, (p + f[i][j], j, k - 1))
+        return -1
+
+if __name__ == '__main__':
+    pass
